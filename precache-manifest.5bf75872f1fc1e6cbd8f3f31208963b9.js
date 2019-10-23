cf3d46fd793506a34eeb488a92aa780c971f20be
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-10-23 15:43:36 +0100

    Updates

---
+++ precache-manifest.5bf75872f1fc1e6cbd8f3f31208963b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4ce43bda0f474f3ca1783aebf66b63",
+    "url": "/Light-Out-Game/index.html"
+  },
+  {
+    "revision": "0fea445d30d337ec05ea",
+    "url": "/Light-Out-Game/static/css/main.83e452a3.chunk.css"
+  },
+  {
+    "revision": "50f504f3b48d9a033137",
+    "url": "/Light-Out-Game/static/js/2.450d1d55.chunk.js"
+  },
+  {
+    "revision": "0fea445d30d337ec05ea",
+    "url": "/Light-Out-Game/static/js/main.c982f48d.chunk.js"
+  },
+  {
+    "revision": "d52089d1ebcbc4ec26c6",
+    "url": "/Light-Out-Game/static/js/runtime-main.10fc064f.js"
+  }
+]);
